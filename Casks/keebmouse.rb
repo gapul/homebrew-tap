397fd20feb3b144f991ccf152be62cf10e091809
gapul <92638132+gapul@users.noreply.github.com>
2026-08-13 15:34:22 +0900

    keebmouse 0.1.0

--- Casks/keebmouse.rb
+++ Casks/keebmouse.rb
@@ -1,6 +1,6 @@
 cask "keebmouse" do
   version "0.1.0"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 "f995a35dd3df75765689afd16d54e54352a4bb0b430efaef144afa8079031c0e"
 
   url "https://github.com/gapul/homebrew-tap/releases/download/v#{version}/keebmouse-#{version}-macos-arm64.zip"
   name "keebmouse"
